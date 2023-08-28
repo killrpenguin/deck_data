5affac7743036a497061b608861c8e69de4c1cf9
killrpenguin <dmcfarland8031@gmail.com>
2023-08-28 11:31:53 -0600

    added proxy pool. Scraper targets object and function finished. working on deck objects.

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from scraper_targets import ddb_objects
-from deck_objects import Deck
-import helper_functions
 
 
-proxy = "http://103.77.60.14:80"
-driver = helper_functions.web_driver(proxy=proxy)
+proxy = 'http://45.225.184.177:999'
 target_objects = ddb_objects()
-decks = []
 for i in target_objects:
-    print(f'{i.deck_link}')
     if 'moxfield' == i.link_group:
-        mx_deck = Deck(deck_link=i.deck_link, link_group=i.link_group,
-                    category=i.category, driver=driver)
-        decks.append(mx_deck)
-        driver.close()
-        print(decks)
+        i.get_mx(proxy=proxy)
+        print(f'{i.deck_link} {i.deck_name} {i.category} {len(i.deck_list)}')
--- scraper_targets.py
+++ scraper_targets.py
@@ -1,23 +1,47 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import random
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import EdgeOptions
 import re
 
 
+
 class Scraper_Targets():
-    def __init__(self, deck_link, link_group=None, category=None):
+    def __init__(self, deck_link, link_group=None, category=None, deck_commander=None, deck_author=None):
         self.wait = random.randint(5, 15)
         self.deck_link = deck_link # URL
-        self.link_group = link_group # ex: moxfield, tappedout, deckstats, arkedekt
+        self.link_group = link_group # ex: This will define which scraper function to use. Moxfield, tappedout, etc.
         self.category = category # competitive or outdated from ddb grouping
+        self.deck_list = []
+        self.deck_name = ''
+        self.deck_commander = deck_commander
+        self.deck_author = deck_author
 
+    def get_mx(self, proxy):
+        edge_options = EdgeOptions()
+        edge_options.use_chromium = True
+        edge_options.add_argument('headless'), edge_options.add_argument('disable-gpu')
+        edge_options.add_argument("--proxy_server=%s" % proxy)
+        driver = selenium.webdriver.Edge(options=edge_options)
+        driver.get(self.deck_link)
+        driver.implicitly_wait(self.wait)
+        self.deck_name = driver.find_element(By.XPATH, "//div[@class='deckheader']"
+                                                  "//div[@class='deckheader-content']/"
+                                                  "/div[@class='container py-5 text-white']"
+                                                  "//form/h1[@class='mb-2']/"
+                                                  "/span[@id='menu-deckname']"
+                                                  "//span[@class='deckheader-name']").text
+        decklist_dirty = driver.find_elements(By.XPATH, "//div[@class='deckview']")
+        decklist = [a.text.strip().split('\n') for a in decklist_dirty]
+        decklist = [a for a in decklist[0]]
+        decklist = [re.sub("^\d*x|[A-Za-z]/[A-Za-z]|^\$.+|^€.+|.+\(\d+.*|"
+                           "[0-9]|^R.+\sH.+|^[A-Za-z].+@.[A-Za-z].+|"
+                           "^V.+\sO.+|^A..\sT.+", '', a) for a in decklist]
+        self.deck_list = [a.strip() for a in decklist if '' != a]
 
-    def __iter__(self):
-        return self
-
-
-    def __next__(self):
-        pass
 
 
 def ddb_objects():
