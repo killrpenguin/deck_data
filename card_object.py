1a45ab3ecf56f1e2c1b5cf24e1762ed60bd4b7cf
killrpenguin <dmcfarland8031@gmail.com>
2023-09-06 20:03:05 -0600

    rewrote card object to better handle api interaction and card face information.

--- card_object.py
+++ card_object.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
 from typing import Dict
 import time
 import json
@@ -7,55 +6,42 @@ import requests
 from bs4 import BeautifulSoup
 
 
-@dataclass()
-class Card:
-    card_name: str
-    mc: str
-    card_type: str
-    card_text: List
-    legal_status: str
-    card_layout: str
-    json : Dict
+@dataclass
+class Face:
+    face_name: str = field(default=str)
+    cmc: float = field(default=float)
+    mana_cost: str = field(default=str)
+    card_type: str = field(default=str)
+    card_text: str = field(default=str)
+    color_ident: str = field(default=str)
 
 
-    def print_card_details(self):
-        print(f'{self.card_name}, {self.mc}, {self.card_type}, {self.legal_status}, {self.card_text}')
+class Card():
+    def __init__(self, card_name):
+        self.card_name = card_name
+        self.card_dict = self.get_json_dict(self.card_name)
+        self.card_layout = self.card_dict['layout']
+        self.faces = self.make_faces(self.card_dict)
+        self.legal_status = self.card_dict['legalities']['commander']
 
-
-    # REMEMBER TO LIMIT TO < 10 REQUESTS PER SECOND
-    def populate_with_api(self, card):
+    def get_json_dict(self, card) -> Dict:
         link = "https://api.scryfall.com/cards/named?fuzzy=" + card
         page = requests.get(link)
         soup = BeautifulSoup(page.text, "html.parser")
-        self.card_dict = json.loads(soup.text)
-        self.card_layout = self.card_dict['layout']
-        try:
-            if self.card_dict['layout'] == 'normal':
-                self.card_name = self.card_dict['name']
-                self.mc = self.card_dict['mana_cost']
-                self.card_type = self.card_dict['type_line']
-                self.card_text = self.card_dict['oracle_text'].split('\n')
-                self.legal_status = self.card_dict['legalities']['commander']
-            elif self.card_dict['layout'] == 'modal_dfc':
-                self.card_name = f"Front: {self.card_dict['card_faces'][0]['name']} Back: {self.card_dict['card_faces'][1]['name']}"
-                self.mc = f"Front: {self.card_dict['card_faces'][0]['mana_cost']} Back: {self.card_dict['card_faces'][1]['mana_cost']}"
-                self.card_type = self.card_dict['type_line']
-                self.card_text = self.card_dict['card_faces'][0]['oracle_text'].split('\n')
-                self.card_text = ["Front: " + text for text in self.card_text]
-                back = self.card_dict['card_faces'][1]['oracle_text'].split('\n')
-                for text in back:
-                    self.card_text.append("Back: " + text)
-                self.legal_status = self.card_dict['legalities']['commander']
-        except Exception as e:
-            print(self.card_dict['name'])
-            print(e)
-
-
+        card_dict = json.loads(soup.text)
         time.sleep(.25)
+        return card_dict
 
-
-"""card = Card('', '', '', [], '')
-proxy = 'http://45.225.184.177:999'
-card.populate_with_api('Mountain')
-print(f'{card.card_name}, {card.card_type}, {card.card_text}, {card.mc}, {card.legal_status}')
-"""
+    def make_faces(self, card) -> list:
+        faces = []
+        try:
+            for side in range(len(card['card_faces'])):
+                faces.append(Face(face_name=card['card_faces'][side]['name'],
+                                  cmc=card['cmc'], mana_cost=card['card_faces'][side]['mana_cost'],
+                                  card_type=card['card_faces'][side]['type_line'],
+                                  card_text=card['card_faces'][side]['oracle_text']))
+        except KeyError:
+            faces.append(Face(face_name=card['name'], cmc=card['cmc'], mana_cost=card['mana_cost'],
+                              card_type=card['type_line'], card_text=card['oracle_text']
+                              ))
+        return faces
